1b7994cb0ff8410b3022373db7625112c1bc9801
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-10-27 17:27:03 -0300

    2.9 a 2.12 Injetando dependências (beans Spring)

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.model.Cliente;
+import com.algaworks.algafood.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService service;
+
+    public MeuPrimeiroController(AtivacaoClienteService service) {
+        this.service = service;
+        System.out.println("MeuPrimeiroController: " + service);
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(){
+        Cliente joao = new Cliente("João", "joao@xyz.com", "19988884444");
+        service.ativar(joao);
+
         return "Olá Linconha!";
     }
 }
---
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood.notificacao;
+
+import com.algaworks.algafood.model.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail implements Notificador {
+
+    public NotificadorEmail(){
+        System.out.println("Construtor chamado: NotificadorEmail");
+    }
+
+    @Override
+    public void notificar(Cliente cliente, String msg){
+        System.out.printf("Notificando %s através do e-mail %s: %s \n",
+                cliente.getNome(), cliente.getEmail(), msg);
+    }
+}
