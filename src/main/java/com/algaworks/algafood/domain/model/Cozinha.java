60e5a54d18606ee0b51a5009ea12094ad7e055fa
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-11-03 17:17:20 -0300

    4.15. Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,6 +11,7 @@ import javax.persistence.*;
 //@Getter
 //@Setter
 //@EqualsAndHashCode
+@JsonRootName("gastronomia")//muda a propriedade (testar no postman)
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)// o parâmetro srve para que o equals e hash seja incluído apenas se for explícito
 @Entity
@@ -18,6 +22,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @JsonIgnore // ignora a propriedade (testar no postman)
+    @JsonProperty("titulo") // a representacao de "nome" será titulo (testar no postman)
     @Column(nullable = false) // este parâmetro nullable=false significa que essas colunas não aceitam valores nulos
     private String nome;
     
