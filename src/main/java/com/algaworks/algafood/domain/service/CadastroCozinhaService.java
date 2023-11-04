7c476da38d2f37f4d2068e00044172cd28178732
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-11-04 16:26:57 -0300

    4.28. Refatorando a exclusão de cozinhas para usar domain services

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -1,8 +1,12 @@
 package com.algaworks.algafood.domain.service;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +15,28 @@ public class CadastroCozinhaService {
     @Autowired
     private CozinhaRepository repository;
 
-    /**
-     * Foi decidido usar este método tanto para salvar quanto para atualizar
-     * não é uma regra, ou seja, pode ter um método para atualizar tbm*/
     public Cozinha salvar(Cozinha cozinha) {
         return repository.salvar(cozinha);
+    }
+
+    public void excluir(Long cozinhaId) {
+        /**Fazemos o tratamento de exceções aqui na classe de serviço
+         * Mas na classe de serviço, não pode ter acesso às implementações
+         * da api.
+         * Por exemplo, retornar um ResponseEntity, não pode ser realizada de forma alguma*/
+        try {
+            repository.remover(cozinhaId);
+
+        } catch (EmptyResultDataAccessException emptyException) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de cozinha com o código %d",
+                            cozinhaId));
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Cozinha de código %d não pode ser removida, pois está em uso",
+                            cozinhaId)); //exception de negócio
+        }
     }
 
 }
