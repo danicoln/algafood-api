d382d46c4d56f7269a9fd064cc04a6e59d428c7b
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-05 20:25:02 -0300

    9.6. Validando as associações de uma entidade em cascata

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -52,6 +53,8 @@ public class Restaurante {
     private LocalDateTime dataAtualizacao;
 
     //    @JsonIgnore
+    @Valid
+    @NotNull
     @ManyToOne//(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
