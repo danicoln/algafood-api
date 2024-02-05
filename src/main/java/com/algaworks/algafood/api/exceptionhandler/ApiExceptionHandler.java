aa9c499d84b3110dacaf9d37b9d0056525da47a7
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-05 20:01:26 -0300

    9.4. Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,6 +24,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -102,8 +104,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente."
                 , ex.getMessage());
 
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+                .map(fieldError -> Problem.Field.builder() // transforma em um stream de Problem.Field
+                        .name(fieldError.getField())
+                        .userMessage(fieldError.getDefaultMessage())
+                        .build())
+                .collect(Collectors.toList());
+
         Problem problem = createProblemBuilder(status, problemType, detail)
-                .userMessage(detail).build();
+                .userMessage(detail)
+                .fields(problemFields).build();
 
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
